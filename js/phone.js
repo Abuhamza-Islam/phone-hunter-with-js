35a269b8038cebad2e8c1b5569cf21593805445c
Abuhamza Islam <abuhamzaislam2023@gmail.com>
2024-09-02 13:48:18 +0600

    finish the api

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText,isShowAll) => {
+const loadPhone = async (searchText = 'iphone',isShowAll) => {
   const res = await fetch(`
      https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
@@ -22,7 +22,7 @@ const displayPhones = (phones,isShowAll) => {
     showAllContainer.classList.add('hidden');
   }
 
-  console.log('is show all: ',isShowAll);
+  // console.log('is show all: ',isShowAll);
   //show the limited search result or display only first 12 phones if not show all
 // console.log(phones.length);
   if(!isShowAll){
@@ -41,8 +41,8 @@ const displayPhones = (phones,isShowAll) => {
                 <div class="card-body">
                     <h2 class="card-title">${phone.phone_name}</h2>
                     
-                    <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <div class="card-actions justify-center">
+                    <button onclick ="handleShowDetails('${phone.slug}')"  class="btn btn-primary">Show Details</button>
                     </div>
                 </div>
        `;
@@ -52,6 +52,46 @@ const displayPhones = (phones,isShowAll) => {
   //hide loading spinner
   toggleLoadingSpinner(false);
 };
+
+//
+const handleShowDetails = async (id) =>{
+  console.log('click show details',id);
+  //load single phone data
+  const res  = await  fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await res.json();
+  const phone = data.data;
+  showPhoneDetails(phone);
+
+}
+
+//
+const showPhoneDetails = (phone)=>{
+  console.log(phone);
+  const phoneName = document.getElementById('show-detail-phone-name');
+     phoneName.classList = 'text-center text-3xl mb-3'
+  phoneName.innerText = phone.name; 
+
+  const showDetailContainer = document.getElementById('show-detail-container');
+  showDetailContainer.classList = "flex flex-col gap-3  justify-center items-center text-left";
+
+
+  showDetailContainer.innerHTML = `
+  <img class="hover:h-96 w-sm" src="${phone.image}" alt=""/>
+
+ <div class="hover:text-3xl">
+    <p><span class="hover:font-bold hover:text-green-500">Storage:</span> ${phone.mainFeatures.storage ? phone.mainFeatures.storage : 'N/A'}</p>
+    <p><span class="hover:font-bold hover:text-green-500">Display:</span> ${phone.mainFeatures.displaySize ? phone.mainFeatures.displaySize : 'N/A'}</p>
+    <p><span class="hover:font-bold hover:text-green-500">Chipset:</span> ${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : 'N/A'}</p>
+    <p><span class="hover:font-bold hover:text-green-500">Memory:</span> ${phone.mainFeatures.memory ? phone.mainFeatures.memory : 'N/A'}</p>    
+</div>
+
+`;
+
+  //show the modal
+  show_details_modal.showModal();
+}
+
+
 
 //get the search value and send it to the loadPhone()
 const getSearchValue =(isShowAll)=>{
@@ -90,4 +130,4 @@ const handleShowAll = () =>{
   getSearchValue(true);
 }
 
-// loadPhone();
+loadPhone();
